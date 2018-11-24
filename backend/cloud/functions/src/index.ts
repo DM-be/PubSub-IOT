a6e252f024b8b29abf0d74011083de968e7487da
MorentDennis <dennis.morent@student.howest.be>
2018-11-24 16:35:34 +0100

    added clound functions setup

--- backend/cloud/functions/src/index.ts
+++ backend/cloud/functions/src/index.ts
@@ -1,4 +1,64 @@
 import * as functions from 'firebase-functions';
+import * as mqtt from 'mqtt';
+let counter = 0;
+
+
+const options = {
+  port: 12295,
+  host: "mqtt://m20.cloudmqtt.com",
+  clientId:
+    "mqttjs_" +
+    Math.random()
+      .toString(16)
+      .substr(2, 8),
+  username: "vqdhgxdd",
+  password: "0zyRZ9ySe5Cn",
+  keepalive: 60,
+  reconnectPeriod: 1000,
+  protocolId: "MQIsdp",
+  protocolVersion: 3,
+  clean: true,
+  encoding: "utf8"
+};
+const client = mqtt.connect(
+  "mqtt://m20.cloudmqtt.com",
+  options
+);
+
+client.on("connect", function() {
+  client.subscribe("soundLevel");
+  client.subscribe("movementDetected");
+});
+
+client.on("message", function(topic, message) {
+  if(topic.toString() === 'movementDetected') {
+    console.log('movement');
+    counter += 1;
+    handleMovementDetection();
+
+  }
+  else if(topic.toString() === 'soundLevel') {
+
+  }
+});
+
+
+function handleMovementDetection() {
+};
+
+function handleSoundLevel(message) {
+
+}
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+ response.send(counter.toString());
+});
+
+
+
+
+
+
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
