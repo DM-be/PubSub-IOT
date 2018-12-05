160ff86a28d99da6c2ad3b136e3fec2009edf91a
MorentDennis <dennis.morent@student.howest.be>
2018-12-05 15:38:50 +0100

    added sending sound with timer to movementdetectioncode

---
+++ arduinos/arduino_A/sernderReviever/sernderReviever.ino
@@ -0,0 +1,43 @@
+
+
+#include <RH_ASK.h>
+#include <SPI.h> // Not actualy used but needed to compile
+#include "Timer.h"
+
+RH_ASK driver;
+
+
+int movementDetectionPin = 3;  
+int val = 0;
+int pirState = LOW;
+
+int soundLevelMeasurementPin = A0;
+
+void setup()
+{
+  pinMode(movementDetectionPin, INPUT);
+  pinMode(soundLevelMeasurementPin, INPUT);
+  
+  attachInterrupt(digitalPinToInterrupt(movementDetectionPin), sendMovementDetection, RISING);
+  t.every(30000,sendSoundLevelMeasurement); // send soundlevelmeasurement every 30 seconds
+    Serial.begin(9600); // Debugging only
+    if (!driver.init()){
+      Serial.println("init failed");
+    }
+}
+
+void loop()
+{
+ t.update(); // let the timer run
+}
+
+void sendMovementDetection(){
+  Serial.println("HIGH");
+  const char *msg = "High World!";
+  driver.send((uint8_t *)msg, strlen(msg));
+}
+
+void sendSoundLevelMeasurement() {
+  int soundLevelMeasurement = analogRead(soundLevelMeasurementPin);
+  driver.send(soundLevelMeasurement);
+  }
