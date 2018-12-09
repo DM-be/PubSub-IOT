cbea7b10f05b282ab0b6ed1eb323c4616a405808
MorentDennis <dennis.morent@student.howest.be>
2018-12-09 11:53:46 +0100

    added firebase config and packes + basic auth service

--- frontend/IoT-group-12-app/src/environments/environment.prod.ts
+++ frontend/IoT-group-12-app/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCsaCCTloyNQYO3C14qiEBkHVoWMAHck5A",
+    authDomain: "hackthefuture-9dc26.firebaseapp.com",
+    databaseURL: "https://hackthefuture-9dc26.firebaseio.com",
+    projectId: "hackthefuture-9dc26",
+    storageBucket: "hackthefuture-9dc26.appspot.com",
+    messagingSenderId: "508170829181"
+  }
 };
