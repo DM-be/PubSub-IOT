04c08ad18b07f85ffd0862c94ca718e21ca03134
MorentDennis <dennis.morent@student.howest.be>
2018-12-10 18:01:57 +0100

    basic sound measurement page with subscription to database

---
+++ frontend/IoT-group-12-app/src/app/sound/sound.page.ts
@@ -0,0 +1,48 @@
+
+import { Component, OnInit } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
+import * as moment from 'moment';
+
+
+
+
+interface SoundLevelMeasurement {
+  timestamp: string | firebase.firestore.Timestamp ; // a string converted from a date (date in the db for orderby) using momentjs
+  soundLevel: string;
+}
+
+
+@Component({
+  selector: "app-sound",
+  templateUrl: "./sound.page.html",
+  styleUrls: ["./sound.page.scss"]
+})
+
+export class SoundPage implements OnInit {
+  public soundLevelMeasurements: SoundLevelMeasurement[];
+
+  constructor(private _firestore: AngularFirestore) {}
+
+  ngOnInit() {
+    this.subscribeToSoundMeasurements();
+  }
+
+  private subscribeToSoundMeasurements() {
+    this._firestore
+      .collection("soundMeasurements", ref => ref.orderBy("timestamp", "desc"))
+      .valueChanges()
+      .subscribe((soundLevelMeasurements: SoundLevelMeasurement[]) => {
+        if (soundLevelMeasurements) {
+          this.soundLevelMeasurements = soundLevelMeasurements.map(slm => {
+            const timestamp = slm.timestamp as firebase.firestore.Timestamp;
+            const ms = timestamp.seconds * 1000;
+            const date = new Date(ms);
+            return {
+              timestamp: moment(date).fromNow(),
+              soundLevel: slm.soundLevel
+            } as SoundLevelMeasurement;
+          });
+        }
+      });
+  }
+}
